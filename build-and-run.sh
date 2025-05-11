2a586539556e36654adc1f1cc391303907c2a4e1
Joaooh98 <joaocarlosdepaiva1998@gmail.com>
2025-05-11 10:36:01 -0300

    execution script

---
+++ build-and-run.sh
@@ -0,0 +1,13 @@
+set -e
+
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo -e "${GREEN}1. Compilando aplicação nativa${NC}"
+./mvnw clean package -Dnative -Dquarkus.native.container-build=true
+
+echo -e "${GREEN}2. Construindo imagem Docker${NC}"
+docker build -f src/main/docker/Dockerfile.native -t quarkus/altice-labs-quarkus .
+
+echo -e "${GREEN}3. Executando contêiner${NC}"
+docker run -i --rm -p 8081:8081 quarkus/altice-labs-quarkus
