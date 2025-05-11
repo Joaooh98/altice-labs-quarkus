03a89da2c8e5dee0da819d7cceddd38b65bb23e8
Joaooh98 <joaocarlosdepaiva1998@gmail.com>
2025-05-11 11:06:49 -0300

    fix: config

---
+++ build-and-run-jvm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo -e "${GREEN}1. Compilando aplicação no modo JVM${NC}"
+./mvnw clean package
+
+echo -e "${GREEN}2. Construindo imagem Docker${NC}"
+docker build -f src/main/docker/Dockerfile.jvm -t quarkus/altice-labs-quarkus-jvm .
+
+echo -e "${GREEN}3. Executando contêiner${NC}"
+docker run -i --rm --name labseq-native -p 8081:8081 quarkus/altice-labs-quarkus-jvm &
